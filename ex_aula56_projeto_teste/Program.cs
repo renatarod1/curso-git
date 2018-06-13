da9db2038df6f1c77e4c6a21d5e99f647b9af4a5
Renata <ticare1@hotmail.com>
2018-06-13 12:41:13 -0300

    Projeto iniciado, mostrando a msg Ola Mundo!

---
+++ ex_aula56_projeto_teste/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ex_aula56_projeto_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+            Console.ReadLine();
+        }
+    }
+}
